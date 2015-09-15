6455dfe6a6a5a8a6a44781d09bf3daaf024942df
liuxh532 <786995209@qq.com>
2015-09-15 21:25:39 +0800

    添加友盟推送

--- Where'sBaby/Where'sBaby/AppDelegate.m
+++ Where'sBaby/Where'sBaby/AppDelegate.m
@@ -7,8 +7,12 @@
 //
 
 #import "AppDelegate.h"
+#import "UMessage.h"
 #import <MAMapKit/MAMapKit.h>
 
+#define UMSYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+
+#define _IPHONE80_ 80000
 @interface AppDelegate ()
 
 @end
@@ -19,7 +23,76 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [MAMapServices sharedServices].apiKey = @"57f662120235dcb0a6ac6dc44425a61d";
+    [UMessage startWithAppkey:@"55f81820e0f55a69c9000f3e" launchOptions:launchOptions];
+    
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= _IPHONE80_
+    if(UMSYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0"))
+    {
+        //register remoteNotification types
+        UIMutableUserNotificationAction *action1 = [[UIMutableUserNotificationAction alloc] init];
+        action1.identifier = @"action1_identifier";
+        action1.title=@"Accept";
+        action1.activationMode = UIUserNotificationActivationModeForeground;//当点击的时候启动程序
+        
+        UIMutableUserNotificationAction *action2 = [[UIMutableUserNotificationAction alloc] init];  //第二按钮
+        action2.identifier = @"action2_identifier";
+        action2.title=@"Reject";
+        action2.activationMode = UIUserNotificationActivationModeBackground;//当点击的时候不启动程序，在后台处理
+        action2.authenticationRequired = YES;//需要解锁才能处理，如果action.activationMode = UIUserNotificationActivationModeForeground;则这个属性被忽略；
+        action2.destructive = YES;
+        
+        UIMutableUserNotificationCategory *categorys = [[UIMutableUserNotificationCategory alloc] init];
+        categorys.identifier = @"category1";//这组动作的唯一标示
+        [categorys setActions:@[action1,action2] forContext:(UIUserNotificationActionContextDefault)];
+        
+        UIUserNotificationSettings *userSettings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge|UIUserNotificationTypeSound|UIUserNotificationTypeAlert
+                                                                                     categories:[NSSet setWithObject:categorys]];
+        [UMessage registerRemoteNotificationAndUserNotificationSettings:userSettings];
+        
+    } else{
+        //register remoteNotification types
+        [UMessage registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge
+         |UIRemoteNotificationTypeSound
+         |UIRemoteNotificationTypeAlert];
+    }
+#else
+    
+    //register remoteNotification types
+    [UMessage registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge
+     |UIRemoteNotificationTypeSound
+     |UIRemoteNotificationTypeAlert];
+    
+#endif
+    
+    //for log
+    [UMessage setLogEnabled:NO];
+    [UMessage setAutoAlert:NO];
+    
     return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    
+    NSString *dtoken = [[[[deviceToken description] stringByReplacingOccurrencesOfString: @"<" withString: @""]
+                         stringByReplacingOccurrencesOfString: @">" withString: @""]
+                        stringByReplacingOccurrencesOfString: @" " withString: @""];
+    [[NSUserDefaults standardUserDefaults] setObject:dtoken forKey:@"deviceToken"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    NSLog(@"%@",dtoken);
+    [UMessage registerDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"%@",userInfo);
+    NSString *str = userInfo[@"aps"][@"alert"];
+    UIAlertView *aview = [[UIAlertView alloc] initWithTitle:@"通知" message:str delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    [aview show];
+    [UMessage didReceiveRemoteNotification:userInfo];
+    NSNotification *notification =[NSNotification notificationWithName:@"receiveRemoteNotification" object:nil userInfo:userInfo];
+    //通过通知中心发送通知
+    [[NSNotificationCenter defaultCenter] postNotification:notification];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
