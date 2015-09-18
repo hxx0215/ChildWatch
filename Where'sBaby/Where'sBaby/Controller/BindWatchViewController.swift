ce1f938d204f4b5b214e3ae13b473705a5ed26a7
liuxh532 <786995209@qq.com>
2015-09-18 15:32:55 +0800

    绑定设备接口

--- Where'sBaby/Where'sBaby/Controller/BindWatchViewController.swift
+++ Where'sBaby/Where'sBaby/Controller/BindWatchViewController.swift
@@ -9,22 +9,36 @@
 import UIKit
 import AVFoundation
 
+enum BindType: String{
+    case InputDeviceID
+    case ScanDeviceID
+    case InputNickName
+    case WaitAdmin
+    case AdminAgree
+    case AdminReject
+}
+
 class BindWatchViewController: UIViewController,AVCaptureMetadataOutputObjectsDelegate {
 
     @IBOutlet weak var inputButton: UIButton!
     @IBOutlet weak var scanButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var bindView: UIView!
     @IBOutlet weak var scanView: UIView!
     @IBOutlet weak var inputIDView: UIView!
     @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var inputLabel: UILabel!
+    @IBOutlet weak var inputTextFieldBGView: UIImageView!
+    var bindType: BindType = .InputDeviceID
+    var bindDeviceID: String?
+    var nickName: String?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.bindView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.8, 0.8)
         IHKeyboardAvoiding.setAvoidingView(self.bindView)
-        self.inputButton.selected = true
-        self.scanButton.selected = false
+        self.bindType = .InputDeviceID
         refreshInputView()
     }
 
@@ -38,23 +52,144 @@ class BindWatchViewController: UIViewController,AVCaptureMetadataOutputObjectsDe
         setupCamera()
     }
     @IBAction func inputClicked(sender: UIButton) {
-        sender.selected = true
-        scanButton.selected = false
+        bindType = .InputDeviceID
         refreshInputView()
         self.session.stopRunning()
     }
     @IBAction func scanClicked(sender: UIButton) {
-        sender.selected = true
-        inputButton.selected = false
+        bindType = .ScanDeviceID
         inputTextField.resignFirstResponder()
         refreshInputView()
         self.session.startRunning()
         self.preview.frame = self.scanView.bounds
     }
+    @IBAction func doneInputClicked(sender: UIButton) {
+        if self.bindType == .InputDeviceID{
+            self.bindDeviceID = self.inputTextField.text!
+            bindType = .InputNickName
+            refreshInputView()
+        }
+        else
+        {
+            self.nickName = self.inputTextField.text!
+            self.inputTextField.resignFirstResponder()
+            doBindDevice()
+        }
+    }
+    
+    func doBindDevice(){
+        let userId = NSUserDefaults.standardUserDefaults().objectForKey("id") as! NSNumber
+        let dic:[String:String] = ["deviceno":self.bindDeviceID!,"nickname":self.nickName!,"userid":userId.stringValue]
+        let hud = MBProgressHUD.showHUDAddedTo(self.view.window, animated: true)
+        hud.labelText = "绑定中"
+        LoginRequest.AddDeviceBindWithParameters(dic, success: { (AnyObject object) -> Void in
+            let dic:NSDictionary = object as! NSDictionary
+            let state:Int = dic["state"] as! Int
+            
+            //0是已经有超级管理员的
+            //7是之前没有管理员，设备初次第一个人绑定，这个人就是超级管理员
+            if(state==0  || state==7)
+            {
+                hud.mode = .Text
+                hud.labelText = "绑定成功";
+                hud.hide(true, afterDelay: 1.5)
+                if state==0{
+                    self.bindType = .WaitAdmin
+                    self.refreshInputView()
+                }
+                else{
+                    let res = dic["data"] as! NSArray
+                    let f = res.firstObject as! [String:AnyObject]
+                    for (key,value) in f{
+                        NSUserDefaults.standardUserDefaults().setObject(value, forKey: key)
+                    }
+                        
+                    self.dismissViewControllerAnimated(true){
+                        
+                    }
+                }
+            }
+            else if(state==1)
+            {
+                
+                hud.mode = .Text
+                hud.labelText = "用户已经和设备绑定";
+                hud.hide(true, afterDelay: 1.5)
+            }
+            else if(state==4)
+            {
+                
+                hud.mode = .Text
+                hud.labelText = "用户不存在";
+                hud.hide(true, afterDelay: 1.5)
+            }
+            else if(state==5)
+            {
+                
+                hud.mode = .Text
+                hud.labelText = "超过设备最大绑定数";
+                hud.hide(true, afterDelay: 1.5)
+            }
+            else if(state==6)
+            {
+                
+                hud.mode = .Text
+                hud.labelText = "设备未启用";
+                hud.hide(true, afterDelay: 1.5)
+            }
+            else
+            {
+                hud.mode = .Text
+                hud.labelText = "服务器内部错误";
+                hud.hide(true, afterDelay: 1.5)
+            }
+            }) { (NSError error) -> Void in
+                print(error)
+                hud.mode = .Text
+                hud.labelText = error.domain;
+                hud.hide(true, afterDelay: 1.5)
+        }
+    }
 
     func refreshInputView(){
-        inputIDView.hidden = !inputButton.selected
-        scanView.hidden = !scanButton.selected
+        inputTextField.resignFirstResponder()
+        inputTextField.text = ""
+        self.inputIDView.hidden = (self.bindType == .ScanDeviceID)
+        self.scanView.hidden = (self.bindType != .ScanDeviceID)
+        inputButton.hidden = !(self.bindType == .ScanDeviceID || self.bindType == .InputDeviceID)
+        scanButton.hidden = !(self.bindType == .ScanDeviceID || self.bindType == .InputDeviceID)
+        switch self.bindType{
+        case .InputDeviceID:
+            inputButton.selected = true;
+            scanButton.selected = false;
+            inputTextField.placeholder = "请输入设备码"
+            doneButton.setTitle("立即绑定", forState: .Normal)
+            inputLabel.hidden = false;
+            inputTextField.keyboardType = .NumberPad
+            inputLabel.text = "请在手表上找到16位绑定ID输入"
+            inputTextField.hidden = false
+            inputTextFieldBGView.hidden = false;
+            doneButton.hidden = false
+        case .ScanDeviceID:
+            inputButton.selected = false
+            scanButton.selected = true
+        case .InputNickName:
+            inputTextField.keyboardType = .Default
+            inputTextField.placeholder = "请输入您的名称"
+            doneButton.setTitle("确定", forState: .Normal)
+            inputLabel.hidden = true
+            inputTextField.hidden = false
+            inputTextFieldBGView.hidden = false;
+            doneButton.hidden = false
+        case .WaitAdmin:
+            inputLabel.text = "您的请求已发送给管理员,请等待管理员处理,如果等待时间过长请联系管理员"
+            inputLabel.hidden = false
+            inputTextField.hidden = true
+            inputTextFieldBGView.hidden = true;
+            doneButton.hidden = true
+        case .AdminAgree: break
+        case .AdminReject: break
+        }
     }
     
     lazy var session: AVCaptureSession = {
