7533f3aedb113b351d8ced607287b7e712942f50
hxx0215 <hxx0215@126.com>
2015-10-13 20:39:50 +0800

    删除重复图片、添加ShareViewController

---
+++ Where'sBaby/Where'sBaby/Controller/ShareViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ShareViewController.swift
+//  Where'sBaby
+//
+//  Created by shadowPriest on 15/10/13.
+//  Copyright © 2015年 coolLH. All rights reserved.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.dismissViewControllerAnimated(false) { () -> Void in
+            
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
